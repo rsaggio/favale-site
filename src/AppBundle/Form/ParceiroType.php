45c236d59f6f6e00748b6def69ed6c2c77f2638d
rsaggio <renan.saggio@gmail.com>
2016-11-03 13:16:01 -0200

    Correcoes

--- src/AppBundle/Form/ParceiroType.php
+++ src/AppBundle/Form/ParceiroType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 
 class ParceiroType extends AbstractType
 {
